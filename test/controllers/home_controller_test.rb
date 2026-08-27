dac9e17b599d4677c71e91af40ffaa95a24cbdd5
Kyle Kestell <kyle@kestell.org>
2026-08-27 08:59:34 -0500

    Link homepage and simplify library seeds

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get root_url
+
     assert_response :success
+    assert_select "a[href=?]", libraries_path, text: "Browse libraries"
   end
 end
